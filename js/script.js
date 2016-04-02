c8642634bc56015f2872502555e6aa63498c4d6a
cotyhsu <chsu520@gmail.com>
2016-04-01 22:31:02 -0700

    polish header

--- js/script.js
+++ js/script.js
@@ -1,9 +1,19 @@
 $(document).ready(function(){
                      
+    $(window).on("scroll", function() {
+        console.log('scroll!');
+        if($(window).scrollTop() > 40 && $(window).width()>768) {
+            $(".headerContainer").addClass("active");
+        } else {
+           $(".headerContainer").removeClass("active");
+        }
+    });
+    
+    
     $( ".fa-bars" ).on('click', function(e) {
       e.preventDefault();
       $( ".mainNav" ).toggleClass("mainNavToggle", 500);
         
     });
     
-})
+});
